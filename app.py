5b6fddd42e6f08707748c3ff75b7ad633baa3bbf
Amulya12182001 <89721049+Amulya12182001@users.noreply.github.com>
2024-07-11 19:39:41 -0400

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,57 @@
-from flask import Flask, request, url_for, redirect, render_template
+# Import necessary modules (Python standard library only)
+import http.server
+import socketserver
+import json
+
+# Load the model from file
+Pkl_Filename = "rf_tuned.pkl" 
+with open(Pkl_Filename, 'rb') as file:  
+    model = pickle.load(file)
+
+# Define the prediction function
+def predict(features):
+    try:
+        # Assuming features are passed as a list of integers
+        final = [features]  # Reshape not needed for single sample prediction
+        pred = model.predict(final)[0]
+        if pred < 0:
+            return 'Error calculating Amount!'
+        else:
+            return f'Expected amount is {pred:.3f}'
+    except Exception as e:
+        return str(e)
+
+# Define the HTTP request handler
+class PredictionHandler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        features = json.loads(post_data.decode('utf-8'))
+        
+        prediction = predict(features)
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(prediction.encode('utf-8'))
+
+# Set up the HTTP server
+def run(server_class=http.server.HTTPServer, handler_class=PredictionHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+# Run the server
+if __name__ == "__main__":
+    run()
+
+
+
+
+
+
+'''from flask import Flask, request, url_for, redirect, render_template
 import pickle
 
 import numpy as np
@@ -30,4 +83,4 @@ def predict():
         return render_template('op.html', pred='Expected amount is {0:.3f}'.format(pred))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)'''
